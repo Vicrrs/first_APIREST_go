ab849455c0bba0bcdb376d0e54f3626f25951141
Vicrrs <enricofermi20192@gmail.com>
2024-03-29 04:50:21 -0300

    definindo struct

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+// Estrutura struct(sao como formularios) para album
+type album struct {
+	ID     string `json: "id"`
+	Title  string `json: "title"`
+	Artist string `json: "artist"`
+	Year   string `json: "year"`
+}
+
 func main() {
 	fmt.Println("Hello, GO!")
 }
