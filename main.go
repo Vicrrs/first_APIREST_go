50787ec488a3233c87fad7c0be03e63cd41df836
Vicrrs <enricofermi20192@gmail.com>
2024-03-29 04:55:35 -0300

    slice para definir os dados armazenados

--- main.go
+++ main.go
@@ -10,6 +10,13 @@ type album struct {
 	Year   string `json: "year"`
 }
 
+// Definindo slice( usadas para armazenar vários valores do mesmo tipo em uma única variável)
+var albums = []album{
+	{ID: "1", Title: "Sou frida", Artist: "Fabio Brazza", Year: "2023"},
+	{ID: "2", Title: "Não é sério", Artist: "CBJR", Year: "2002"},
+	{ID: "3", Title: "Manic", Artist: "Flash Dance", Year: "1980"},
+}
+
 func main() {
 	fmt.Println("Hello, GO!")
 }
