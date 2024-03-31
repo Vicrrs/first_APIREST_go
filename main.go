33e1eb7d7009d83483b00e7284bf49963214ac52
Vicrrs <enricofermi20192@gmail.com>
2024-03-31 00:41:37 -0300

    adicionando get por id e tratamento de erro

--- main.go
+++ main.go
@@ -32,17 +32,32 @@ func getAlbums(c *gin.Context) {
 func postAlbums(c *gin.Context) {
 	var newAlbum album
 
-	c.BindJSON(&newAlbum)
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
 
 	albums = append(albums, newAlbum)
 
 	c.IndentedJSON(http.StatusCreated, albums)
 }
 
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"messagem": "Album nao encontrado"})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 
 	router.Run("localhost:8080")
 }
